265105d53265480553252f1154b4ff0b0804948b
Nahuel Amaya <nahuelbgs@hotmail.com>
2022-04-14 15:50:00 -0300

    Agregue funciones

--- js/main.js
+++ js/main.js
@@ -1,34 +1,12 @@
-// let tipoDeProducto = prompt("¿Que tipo de producto queres comprar? (remera / hoodie)");
-// let cantidad = parseInt(prompt("¿Cuantos productos querés comprar? (1, 2, 3...)"));
-// let precioHoodies = 8990;
-// let precioRemeras = 2990;
-// let resultado = 0;
-
-/*Calcula el precio total segun el tipo de producto y la cantidad ingresada*/
-
-// switch (tipoDeProducto) {
-//     case "hoodie":
-//         resultado = precioHoodies * cantidad;
-//         alert(resultado);
-//         break;
-//     case "remera":
-//         resultado = precioRemeras * cantidad;
-//         alert(resultado);
-//         break;
-//     default:
-//         alert("No ingresaste un tipo de producto válido");
-// }
-
 /*Calcula las cuotas (sin interes) dado un precio del producto y la cantidad de cuotas*/
 
 // function calcularCuotas(precioProducto, cantidadCuotas){
 //     return(precioProducto / cantidadCuotas)
 // }
 
-// ------------------------------------- DESAFIO INCORPORAR ARRAYS ----------------------------------------
 
-class Producto{
-    constructor (nombre, precio){
+class Producto {
+    constructor(nombre, precio) {
         this.nombre = nombre;
         this.precio = precio;
     }
@@ -39,17 +17,35 @@ let listaDeProductos = [];
 const agregarAlCarrito = () => {
     let nombre = prompt("Nombre del producto");
     let precio = parseInt(prompt("Precio del producto"));
-    let prod = new Producto(nombre,precio);
+    let prod = new Producto(nombre, precio);
     listaDeProductos.push(prod);
 }
 
-const eliminarDelCarrito = (prod) => {
-    let index = listaDeProductos.indexOf(prod);
-    if(index != -1) {
-        listaDeProductos.splice(index, 1)
-    }
+const sumarCarrito = () => {
+    let precioTotal = 0;
+    listaDeProductos.forEach((el) => {
+        precioTotal = precioTotal + el.precio;
+    })
+    console.log(precioTotal);
 }
 
-const estaEnElCarrito = (prod) => {
-    listaDeProductos.includes(prod);
+const aplicarDescuento = (precioActual, descuento) => {
+    descontar = precioActual * descuento / 100;
+    precioNuevo = precioActual - descontar;
+    console.log(precioNuevo);
 }
+
+// NO FUNCIONA
+
+const eliminarDelCarrito = (producto) => {
+    let index = listaDeProductos.indexOf(producto);
+    if (index != -1) {
+        listaDeProductos.splice(index, 1)
+    }
+    console.log(listaDeProductos)
+}
+
+listaDeProductos.push (new Producto("tele", 2000));
+
+console.log(listaDeProductos.indexOf("tele"));
+
