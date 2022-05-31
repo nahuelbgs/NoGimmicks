790174c462d9ded07f7cd200102bcb716f14fa45
Nahuel Amaya <nahuelbgs@hotmail.com>
2022-05-31 02:59:52 -0300

    Primera parte del proyecto final

--- js/main.js
+++ js/main.js
@@ -1,50 +1,20 @@
 class Producto {
-    constructor(nombre, precio, imagen) {
+    constructor(nombre, precio, imagen, id) {
         this.nombre = nombre;
         this.precio = precio;
         this.imagen = imagen;
+        this.id = id;
     }
 }
 
 let listaDeProductos = [];
-
-
-/* HECHO SOLO PARA LA ENTREGA */
-const obtenerDatos = () =>{
-    fetch("../datos.json")
-    .then(response => response.json())
-    .then((result) => {
-        let datos = result;
-        datos.forEach(prod => {
-            productosDelCarritoContainer.innerHTML += `<div class="row shoppingCartItem border-bottom">
-        <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 pb-2 pt-3">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate m-2 justify-content-center">${prod.nombre}</h6>
-            </div>
-        </div>
-        <div class="col-2 d-flex justify-content-center">
-            <div class="shopping-cart-price d-flex align-items-center h-100 pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${prod.precio}</p>
-            </div>
-        </div>
-        <div class="col-4">
-        <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 pb-2 pt-3">
-            <input class="shopping-cart-quantity-input cantidad" type="number"
-            value="1">
-            <button class="btn btn-danger" id="buttonDelete" type="button">X</button>
-        </div>
-    </div>`
-        })
-    })
-    .catch(error => console.log(error));
-}
 
 const totalPrice = document.querySelector('.totalPrice');
 const productosDelCarritoContainer = document.querySelector('.productosDelCarritoContainer');
 const botonAgregarAlCarrito = document.querySelectorAll('#comprar');
 
 botonAgregarAlCarrito.forEach((el) => {
-    el.addEventListener('click', obtenerDatos)
+    el.addEventListener('click', agregarAlCarritoClicked)
 });
 
 const guardarLocal = (clave, valor) => {
@@ -61,10 +31,12 @@ function agregarAlCarritoClicked(event) {
     const prodName = box.querySelector('.product-name').textContent;
     const prodPrice = box.querySelector('.prod-price').textContent;
     const prodImg = box.querySelector('.product-img').src;
+    const prodID = box.getAttribute('id');
     const prod = new Producto(prodName, prodPrice, prodImg, prodID);
     listaDeProductos.push(prod);
     guardarLocal(prod.nombre, JSON.stringify(prod));
     agregarProdAlCarritoVisible(listaDeProductos);
+    swal("¡Producto añadido al carrito con éxito!", `${prodName}`, "success");
     actualizarCarrito();
 }
 
@@ -73,11 +45,26 @@ function agregarProdAlCarritoVisible(localStorage) {
         prodName = el.nombre;
         prodPrice = el.precio;
         prodImg = el.imagen;
+        prodID = el.id
     });
 
+    // ------------------------------- NO FUNCIONA -----------------------------------------------
+    // const nombreDelProducto = document.querySelectorAll('.shoppingCartItemTitle').textContent;
+    // const nombreDeLosProductos = productosDelCarritoContainer.getElementsByClassName('shoppingCartItemTitle');
+    
+    // for (let i = 0; i < nombreDeLosProductos.length; i++) {
+    //     if (nombreDeLosProductos[i].innerText === nombreDelProducto) {
+    //         let cantidadDelMismoElemento = nombreDeLosProductos[i
+    //         ].parentElement.parentElement.parentElement.querySelector('.cantidad');
+    //         cantidadDelMismoElemento.value++;
+    //         actualizarCarrito();
+    //         return;
+    //     }
+    // }
+    // ---------------------------------------------------------------------------------------------
     const filaDelCarrito = document.createElement('div');
     const contenidoDelCarrito = `
-    <div class="row shoppingCartItem border-bottom">
+    <div class="row shoppingCartItem border-bottom" id="${prodID}">
         <div class="col-6">
             <div class="shopping-cart-item d-flex align-items-center h-100 pb-2 pt-3">
                 <img src=${prodImg} class="shopping-cart-image">
@@ -91,7 +78,7 @@ function agregarProdAlCarritoVisible(localStorage) {
         </div>
         <div class="col-4">
         <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 pb-2 pt-3">
-            <input class="shopping-cart-quantity-input cantidad" type="number"
+            <input class="cantidad" type="number"
             value="1">
             <button class="btn btn-danger" id="buttonDelete" type="button">X</button>
         </div>
@@ -99,7 +86,7 @@ function agregarProdAlCarritoVisible(localStorage) {
     filaDelCarrito.innerHTML = contenidoDelCarrito;
     productosDelCarritoContainer.append(filaDelCarrito);
     filaDelCarrito.querySelector('.cantidad').addEventListener('change', cambiarCantidad);
-    filaDelCarrito.addEventListener('click', eliminarDelCarrito);
+    filaDelCarrito.querySelector('#buttonDelete').addEventListener('click', eliminarDelCarrito);
 }
 
 const actualizarCarrito = () => {
@@ -121,7 +108,6 @@ const cambiarCantidad = (event) => {
     actualizarCarrito();
 }
 
-
 const buscarEnLocalStorage = () => {
     for (let i = 0; i < localStorage.length; i++){
         let clave = localStorage.key(i);
@@ -133,7 +119,6 @@ const buscarEnLocalStorage = () => {
 
 function eliminarDelCarrito(event) {
     const button = event.target;
-    button.closest('.shoppingCartItem').remove();
     const prodName = document.querySelector(".shoppingCartItemTitle").textContent;
     const index = listaDeProductos.findIndex(prod => prod.nombre === prodName);
     if (index != -1) {
@@ -141,6 +126,7 @@ function eliminarDelCarrito(event) {
         actualizarCarrito()
         eliminarLocal(prodName, buscarEnLocalStorage());
     };
+    button.closest('.shoppingCartItem').remove();
 }
 
 
